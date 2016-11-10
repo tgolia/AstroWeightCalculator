5cf4d6a89faa3ffa7451fbdad6a669de57bc8927
Travis Golia <travis@gigtown.com>
2016-11-10 14:04:20 -0800

    minor changes to JS

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-var planets = [
-    { planet: 'Sun', gravity: 27.9 },
-    { planet: 'Mercury', gravity: 0.377 },
-    { planet: 'Venus', gravity: 0.9032 },
-    { planet: 'Earth', gravity: 1 },
-    { planet: 'Moon', gravity: 0.1655 },
-    { planet: 'Mars', gravity: 0.3895 },
-    { planet: 'Jupiter', gravity: 2.640 },
-    { planet: 'Saturn', gravity: 1.139 },
-    { planet: 'Uranus', gravity: 0.917 },
-    { planet: 'Neptune', gravity: 1.148 },
-    { planet: 'Pluto', gravity: 0.06 }
+var planets = [                             //creates an array of objects for each planet
+    { name: 'Sun', gravity: 27.9 },
+    { name: 'Mercury', gravity: 0.377 },
+    { name: 'Venus', gravity: 0.9032 },
+    { name: 'Earth', gravity: 1 },
+    { name: 'Moon', gravity: 0.1655 },
+    { name: 'Mars', gravity: 0.3895 },
+    { name: 'Jupiter', gravity: 2.640 },
+    { name: 'Saturn', gravity: 1.139 },
+    { name: 'Uranus', gravity: 0.917 },
+    { name: 'Neptune', gravity: 1.148 },
+    { name: 'Pluto', gravity: 0.06 }
 ];
 
-function roundToDecimal(num,dec) {
+function roundToDecimal(num,dec) {        //custom function that rounds number num to decimal dec that you specify
       var rounded = (Math.round(num * Math.pow(10,dec)) / Math.pow(10,dec)).toFixed(dec);
       return rounded;
     }
 
 //1. How to dynamically generate the select element
 
-for (var i = 0; i < planets.length; i++) {
-    var planet = planets[i];
+for (var i = 0; i < planets.length; i++) {  //iterates through objects in planets array
+    var planet = planets[i];                  //creates variable for 'name' property of object
 
-    var optionElement = document.createElement('option'); //creates planet list elements
-    optionElement.value = planet.gravity;
-    optionElement.innerHTML = planet.planet;
+    var optionElement = document.createElement('option'); //creates planet list element objects
+    optionElement.value = planet.gravity;                 //assigns value to list element object
+    optionElement.innerHTML = planet.name;                //assigns name to list element object
 
     var selectElement = document.getElementById('selectPlanet'); //adds planets list elements to list
-    selectElement.appendChild(optionElement);
+    selectElement.appendChild(optionElement);                    //adds list element objects to list
 
     //2. Bind a function to the click event
-    var button = document.getElementById('calculateWeight');
+    var button = document.getElementById('calculateWeight');    //names button variable equal to html button element
 
-    button.onclick = function() {
-        var inputWeight = document.getElementById('inputWeight');
+    button.onclick = function() {          //function to execute certain commands upon the button being clicked
+        var inputWeight = document.getElementById('inputWeight'); //
 
         var weight = inputWeight.value;
 
